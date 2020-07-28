3b28aa53622e96b1437aba627525a4bbbecd0a97
yykhomenko <yykhomenko@gmail.com>
2020-07-28 13:25:43 +0300

    ref

--- algoritms/implementation/gradingStudents/main.go
+++ algoritms/implementation/gradingStudents/main.go
@@ -2,8 +2,8 @@ package main
 
 func gradingStudents(grades []int32) []int32 {
 	for i := 0; i < len(grades); i++ {
-		if grades[i] > 37 && grades[i]%5 != 0 {
-			if 5-int(grades[i]%5) < 3 {
+		if grades[i] >= 38 {
+			if grades[i]%5 > 2 {
 				grades[i] += 5 - grades[i]%5
 			}
 		}
