2ba91a1efbe2842b59c233e3f50afff4e7fc89e3
yykhomenko <yykhomenko@gmail.com>
2020-08-20 13:22:50 +0300

    added kangaroo

---
+++ algoritms/implementation/kangaroo/main.go
@@ -0,0 +1,10 @@
+package main
+
+func kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
+	for i := int32(0); i < 10000; i++ {
+		if v1*i+x1 == v2*i+x2 {
+			return "YES"
+		}
+	}
+	return "NO"
+}
