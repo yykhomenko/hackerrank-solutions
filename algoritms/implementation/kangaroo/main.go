df1dce3039f5be4485bc802f8acf90d34ef79232
yykhomenko <yykhomenko@gmail.com>
2020-08-21 12:12:48 +0300

    added kangarooBetter

--- algoritms/implementation/kangaroo/main.go
+++ algoritms/implementation/kangaroo/main.go
@@ -9,3 +9,11 @@ func kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
 
 	return "NO"
 }
+
+func kangarooBetter(x1 int32, v1 int32, x2 int32, v2 int32) string {
+	if v1 > v2 && (x2-x1)%(v1-v2) == 0 {
+		return "YES"
+	}
+
+	return "NO"
+}
