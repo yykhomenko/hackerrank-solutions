4bfbc48e775974d0a96728d76126862c1b35dcf2
yykhomenko <yykhomenko@gmail.com>
2020-12-13 23:02:42 +0200

    ref

--- algoritms/recursion/recursiveDigitSum/main.go
+++ algoritms/recursion/recursiveDigitSum/main.go
@@ -7,18 +7,16 @@ import (
 func superDigit(n string, k int32) int32 {
 	sum := calc(n)
 	sum *= k
-	n = strconv.Itoa(int(sum))
-	return calc(n)
+	return calc(strconv.Itoa(int(sum)))
 }
 
 func calc(n string) int32 {
 	if len(n) == 1 {
 		return int32(n[0] - '0')
 	}
-	var sum int
+	sum := 0
 	for _, c := range n {
 		sum += int(c) - '0'
 	}
-	n = strconv.Itoa(int(sum))
-	return calc(n)
+	return calc(strconv.Itoa(sum))
 }
