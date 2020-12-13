023b15f4daf31b9af0be67ca462a967f424ed9a4
yykhomenko <yykhomenko@gmail.com>
2020-12-13 18:58:40 +0200

    TestSuperDigit

---
+++ algoritms/recursion/recursiveDigitSum/main.go
@@ -0,0 +1,6 @@
+package main
+
+// Complete the superDigit function below.
+func superDigit(n string, k int32) int32 {
+
+}
---
+++ algoritms/recursion/recursiveDigitSum/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestSuperDigit(t *testing.T) {
+	tests := []struct {
+		args string
+		want int32
+	}{
+		{"0", 0},
+		{"5", 5},
+		{"9875", 2},
+		{"9875987598759875", 8},
+	}
+	for _, tt := range tests {
+		if got := superDigit(tt.args, int32(len(tt.args))); got != tt.want {
+			t.Errorf("superDigit() = %v, want %v", got, tt.want)
+		}
+	}
+}
