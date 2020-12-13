39ab16b4a32f56fa729ebb0ef0753de76274debf
yykhomenko <yykhomenko@gmail.com>
2020-12-13 22:59:31 +0200

    complete

--- algoritms/recursion/recursiveDigitSum/main.go
+++ algoritms/recursion/recursiveDigitSum/main.go
@@ -15,9 +15,9 @@ func calc(n string) int32 {
 	if len(n) == 1 {
 		return int32(n[0] - '0')
 	}
-	var sum int32
+	var sum int
 	for _, c := range n {
-		sum += c - '0'
+		sum += int(c) - '0'
 	}
 	n = strconv.Itoa(int(sum))
 	return calc(n)
