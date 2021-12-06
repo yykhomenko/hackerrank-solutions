ca53fde4f2a905437ac4a729fd0e8044cfd8a039
yykhomenko <yykhomenko@gmail.com>
2021-12-06 10:39:10 +0200

    added func repeatedString

--- algoritms/warmup/repeatedString/main.go
+++ algoritms/warmup/repeatedString/main.go
@@ -1,6 +1,14 @@
 package main
 
-func repeatedString(s string, n int64) int64 {
-	// Write your code here
+import "strings"
 
+func repeatedString(s string, n int64) int64 {
+	l := int64(len(s))
+	part := n / l
+	rest := n % l
+
+	numAPart := int64(strings.Count(s, "a"))
+	numARest := int64(strings.Count(s[:rest], "a"))
+
+	return part*numAPart + numARest
 }
